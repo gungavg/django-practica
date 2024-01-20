10af301849cffe7bbc25ab9f8a7189b633734b63
gungavg <velgar.ja@gmail.com>
2024-01-19 20:08:10 -0600

    haciendo contenido dinamico

--- holamundo/holamundo/views.py
+++ holamundo/holamundo/views.py
@@ -2,3 +2,11 @@
 from django.http import HttpResponse
 def saludo(request):
     return HttpResponse("Hola amiwos del internet")
+
+
+#rutas con parametros
+def adulto(request,edad):
+    if edad >=18:
+        return HttpResponse("eres mayor de edad")
+    else:
+        return HttpResponse("no eres mayor de edad")
--- plantillas/plantillas/urls.py
+++ plantillas/plantillas/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for holamundo project.
+URL configuration for plantillas project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -20,6 +20,6 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('saludo/', views.saludo, name= 'saludo'),
-    path('adulto/<int:edad>',views.adulto, name='adulto')
+    path('simple/', views.simple, name='simple'),
+    path('dinamico/<str:name>', views.dinamico,name='dinamico')
 ]
