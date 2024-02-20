ce881235c3c41dfeb0f2457c71818ba4d38373cb
gungavg <velgar.ja@gmail.com>
2024-02-20 11:17:45 -0600

    se realizo el modelado de datos de una compania

---
+++ practicas pequeñas/practica_modelado_de_datos/practica_modelado_de_datos/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for practica_modelado_de_datos project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'practica_modelado_de_datos.settings')
+
+application = get_asgi_application()
