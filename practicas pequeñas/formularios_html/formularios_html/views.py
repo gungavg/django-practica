3de0dd2b4c6b5570d522e409fc08738cab8f76f8
gungavg <velgar.ja@gmail.com>
2024-02-20 21:02:51 -0600

    se empezo con los formularios

---
+++ practicas pequeñas/formularios_html/formularios_html/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+def form(request):
+    return render(request, 'form.html',{})
+
+def goal(request):
+    if request.method!='GET':
+        return HttpResponse("el metodo post no esta soportado para esta rurta")
+    name =request.GET['name']
+    return render(request,'success.html',{'name':name})
+
